96e666da9137d92998e8c75648bad6532c605786
William Rankin <code@william.bydasein.com>
2019-12-06 11:11:19 +1000

    Initial

---
+++ freeze-it.el
@@ -0,0 +1,81 @@
+;;; freeze-it.el --- Minor mode to make your previous writing read-only  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Paul W. Rankin
+
+;; Author: Paul W. Rankin <pwr@x2510.com>
+;; Keywords: wp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defgroup freeze-it
+  nil
+  "Options for freeze minor mode."
+  :prefix "freeze-it-"
+  :group 'text)
+
+(defcustom freeze-it-delay
+  5
+  "Integer of seconds of idle time before freezing text."
+  :type 'number
+  :safe 'numberp
+  :group 'freeze-it)
+
+(defcustom freeze-it-go-back
+  'line
+  "Amount of text to go back before freezing."
+  :type '(choice (const :tag "None" nil)
+                 (const :tag "Word" word)
+                 (const :tag "Line" line)
+                 (const :tag "Visible Line" visible-line)
+                 (const :tag "Paragraph" paragraph))
+  :safe 'symbolp
+  :group 'freeze-it)
+
+(defcustom freeze-it-lighter
+  " FREEZE!"
+  "Mode-line indicator for `freeze-it-mode'."
+  :type '(choice (const :tag "No lighter" "") string)
+  :safe 'stringp
+  :group 'freeze-it)
+
+(defun freeze-it-now ()
+  (when freeze-it-mode
+    (save-excursion
+      (save-restriction
+        (widen)
+        (when freeze-it-go-back
+          (funcall (intern (concat "forward-" (symbol-name freeze-go-back)))
+                   -1))
+        (put-text-property (point-min) (point) 'read-only t)))))
+
+(define-minor-mode freeze-it-mode
+  "When enabled, after idle timer `freeze-it-delay', text in the current
+buffer before point is made read-only.
+
+The option `freeze-it-go-back' controls how far back before point to
+leave alone."
+  :init-value nil
+  :lighter freeze-it-lighter
+  (if freeze-it-mode
+      (run-with-idle-timer freeze-it-delay t #'freeze-it-now)
+    (cancel-function-timers #'freeze-it-now)))
+
+(provide 'freeze-it)
+;;; freeze-it.el ends here
