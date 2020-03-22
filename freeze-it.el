6cfe9915670532443c068d58c89466d7d83e6f39
William Rankin <code@william.bydasein.com>
2020-03-22 14:07:57 +1000

    Fix type and refill README/Commentary

--- freeze-it.el
+++ freeze-it.el
@@ -25,25 +25,27 @@
 
 ;; # Freeze It #
 
-;; An Emacs minor mode to kill your inner editor! Every writer struggles between
-;; their creative and critical sides, with progress frequently hindered by the
-;; temptation to go back and revise to get things *just right*.
+;; An Emacs minor mode to kill your inner editor! Every writer struggles
+;; between their creative and critical sides, with progress frequently
+;; hindered by the temptation to go back and revise to get things *just
+;; right*.
 
-;; Freeze It aims to combat this tempation. After an idle delay freeze-it-delay
-;; all text between point-min and a configurable distance before point will be
-;; made read-only.
+;; Freeze It aims to combat this temptation. After an idle delay
+;; freeze-it-delay all text between point-min and a configurable
+;; distance before point will be made read-only.
 
-;; Option freeze-it-go-back controls how far this distance "goes back" before
-;; freezing text. This can be nil, word, line, visible-line, line, or
-;; paragraph.
+;; Option freeze-it-go-back controls how far this distance "goes back"
+;; before freezing text. This can be nil, word, line, visible-line,
+;; line, or paragraph.
 
 ;; Command freeze-it-show will momentarily highlight read-only text in
 ;; the buffer. The highlighting uses freeze-it-show face and remains for
 ;; freeze-it-show-delay seconds.
 
-;; Text remains read-only until you kill the buffer, so that you can't cheat. This
-;; is by design, because the minor mode targets the psychological *temptation* to
-;; revise your writing, rather than just the ability.
+;; Text remains read-only until you kill the buffer, so that you can't
+;; cheat. This is by design, because the minor mode targets the
+;; psychological *temptation* to revise your writing, rather than just the
+;; ability.
 
 ;;; Code:
 
